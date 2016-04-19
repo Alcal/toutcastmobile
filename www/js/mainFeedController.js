96e79044447afe79e7405d4091823f11b7f85143
alejandrocastrolopez <acalo91@gmail.com>
2016-04-19 00:16:15 -0700

    Added Approved step to Redeeming Process. Improved Slide aspect. Removed dummy Coupon cards from Landing.

--- www/js/mainFeedController.js
+++ www/js/mainFeedController.js
@@ -1,10 +1,19 @@
 angular.module('toutcast.controllers.feed',[])
 
-.controller('MainFeedCtrl', function($scope, $stateParams, ToutService, ionicMaterialMotion, ionicMaterialInk){
+.controller('MainFeedCtrl', function($scope, $stateParams, ToutService, ionicMaterialMotion, ionicMaterialInk, $timeout){
 
 
 	ToutService.all(function(touts){	
 		$scope.touts = touts;
-		ionicMaterialInk.displayEffect({duration:600});
+		$timeout(function() 
+		{
+	        ionicMaterialMotion.fadeSlideInRight({
+	            startVelocity: 3000
+	        });
+			ionicMaterialInk.displayEffect({duration:600});
+	    }, 700);
 	});
+
+
+	
 });
--- www/js/slide.js
+++ www/js/slide.js
@@ -1,7 +1,7 @@
 angular.module('toutcast.controllers.slide',['ngAnimate'])
 
 .controller('SlideCtrl', 
-	function($scope, $stateParams, $ionicScrollDelegate, ionicMaterialInk)
+	function($scope, $stateParams, $ionicScrollDelegate, ionicMaterialInk, $timeout)
 	{
 		// $scope.$on('$ionicView.enter', function(e) {
 		// 	//$ionicScrollDelegate.
@@ -17,7 +17,10 @@ angular.module('toutcast.controllers.slide',['ngAnimate'])
 			$scope.crescentStyle.height = newValue*2.1+'px';
 			$scope.crescentStyle.width = newValue*2.1+'px';
 			if(newValue>500)
+			{
 				$scope.loadingTout = true;
+				$scope.approve();
+			}
 		});
 
 		$scope.onSleighRelease = function(e)
@@ -37,13 +40,15 @@ angular.module('toutcast.controllers.slide',['ngAnimate'])
 
 		$scope.disableScrolling = function()
 		{
-			$scope.slideTrack.getScrollView().freezeShut(true);
+			$scope.slideTrack.getScrollView().freeze(true);
+			$scope.slideTrack.freezeScroll(true);
 			$scope.scrollFrozen = true;
 		};
 
 		$scope.enableScrolling = function()
 		{
-			$scope.slideTrack.getScrollView().freezeShut(false);
+			$scope.slideTrack.getScrollView().freeze(false);
+			$scope.slideTrack.freezeScroll(false);
 			$scope.scrollFrozen = false;
 		};
 
@@ -58,14 +63,18 @@ angular.module('toutcast.controllers.slide',['ngAnimate'])
 			height:'0px'
 		};
 
-
-		$scope.onDragUp = function(){
-			//$scope.grown = true;
-			// $scope.crescentStyle.height = slideTrack.getScrollPosition().top*2.1+'px';
-			// $scope.crescentStyle.width = slideTrack.getScrollPosition().top*2.1+'px';
-
-		};
   		ionicMaterialInk.displayEffect({duration:600});
+
+  		$scope.approve = function()
+  		{
+  			$timeout(function()
+  				{
+  					$scope.approved = true;
+  					$scope.loadingTout = false;
+  					$scope.slideTrack.scrollTop(true);
+  				}
+  			,4000);
+  		}
 
 	
 });
